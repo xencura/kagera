a9bd5442139f1ea9158f1489f4b121f86ce52fe8
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2016-05-04 12:33:48 +0200

    Some documentation

--- api/src/main/scala/io/kagera/api/colored/Place.scala
+++ api/src/main/scala/io/kagera/api/colored/Place.scala
@@ -4,12 +4,29 @@ object Place {
   def apply[A](id: Long, label: String) = PlaceImpl[A](id, label)
 }
 
+/**
+ * A Place in a colored petri net.
+ */
 trait Place {
 
+  /**
+   * The type or 'color' of this place.
+   */
   type Color
 
+  /**
+   * The unique identifier of this place.
+   *
+   * @return
+   *   A unique identifier.
+   */
   def id: Long
-  def label: String
 
-  override def toString = label
+  /**
+   * A human readable label of this place.
+   *
+   * @return
+   *   The label.
+   */
+  def label: String
 }
--- api/src/main/scala/io/kagera/api/colored/Transition.scala
+++ api/src/main/scala/io/kagera/api/colored/Transition.scala
@@ -4,17 +4,50 @@ import scala.concurrent.Future
 import scala.reflect.runtime.{ universe => ru }
 import scalax.collection.edge.WLDiEdge
 
+/**
+ * A transition in a colored petri net.
+ */
 trait Transition {
 
+  /**
+   * The input type of this transition.
+   */
   type Input
+
+  /**
+   * The output type of this transition.
+   */
   type Output
 
+  /**
+   * The unique identifier of this transition.
+   *
+   * @return
+   *   The unique identifier.
+   */
   def id: Long
+
+  /**
+   * A human readable label of this transition.
+   *
+   * @return
+   *   The label.
+   */
   def label: String
 
-  override def toString = label
-
+  /**
+   * Creates a valid input type for the transition using the in-adjacent (place, arc, tokens) tuples and optional
+   * trigger data.
+   *
+   * @param input
+   *   The in-adjacent (place, arc, tokens) tuples.
+   * @param data
+   *   An optional data field that is given from outside the process.
+   * @return
+   *   A valid instance of type Input.
+   */
   def createInput(input: Seq[(Place, WLDiEdge[Node], Seq[Any])], data: Option[Any]): Input
+
   def createOutput(output: Output, places: Seq[(WLDiEdge[Node], Place)]): ColoredMarking
 
   def apply(input: Input): Future[Output]
