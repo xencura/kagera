c035befbff26f1aa5d36365aebc511ffb5721016
Merlijn van Ittersum <merlijn.van.ittersum@ing.com>
2016-05-25 15:05:38 +0200

    moved ColoredPetriNetInstance related tests to its own spec

--- api/src/test/scala/io/kagera/api/colored/ColoredMarkingSpec.scala
+++ api/src/test/scala/io/kagera/api/colored/ColoredMarkingSpec.scala
@@ -1,36 +1,14 @@
 package io.kagera.api.colored
 
 import io.kagera.api._
-import io.kagera.api.colored.dsl._
 import org.scalatest.Matchers._
 import org.scalatest.WordSpec
-
-import scala.concurrent.duration._
-import scala.concurrent.{ Await, Future }
-import scalax.collection.edge.WLDiEdge
 
 class ColoredMarkingSpec extends WordSpec {
 
   val p1 = Place[Int](id = 1, label = "p1")
   val p2 = Place[String](id = 2, label = "p2")
   val p3 = Place[Double](id = 3, label = "p3")
-
-  val t1 = new AbstractTransition[Any, Any](4, "t1", isManaged = false) {
-
-    override def createOutput(output: Output, outAdjacent: Seq[(WLDiEdge[Node], Place)]): ColoredMarking =
-      outAdjacent.map { case (edge, place) =>
-        place -> Seq(output)
-      }.toMap
-
-    override def createInput(
-      inAdjacent: Seq[(Place, WLDiEdge[Node], Seq[Any])],
-      data: Option[Any],
-      context: TransitionContext
-    ): Input = data.orNull
-
-    override def apply(input: Input)(implicit executor: scala.concurrent.ExecutionContext): Future[Output] =
-      Future.successful(input)
-  }
 
   "A Colored Marking" should {
 
@@ -65,48 +43,9 @@ class ColoredMarkingSpec extends WordSpec {
     "in case of token value equality only consume tokens equal to the multiplicity" in {
 
       val m1: ColoredMarking = Map(p1 -> Seq(1, 1, 1, 1, 1))
-
       val m2: ColoredMarking = Map(p1 -> Seq(1, 1))
 
       m1.consume(m2) shouldBe Map(p1 -> Seq(1, 1, 1))
-    }
-
-    "should only contain the last state" in {
-
-      val initialMarking: ColoredMarking = Map(p1 -> Seq(null))
-
-      val petriNet = process(p1 ~> t1, t1 ~> p2)
-      val instance = processInstance(petriNet, initialMarking, java.util.UUID.randomUUID())
-
-      instance.marking shouldBe initialMarking
-
-      Await.result(instance.fireTransition(t1), 2 seconds) shouldBe Map(p2 -> Seq(null))
-    }
-
-    "should contain all accumulated state (even is marking is already consumed)" in {
-
-      val initialMarking: ColoredMarking = Map(p1 -> Seq(null))
-
-      val petriNet = process(p1 ~> t1, t1 ~> p2)
-      val instance = processInstance(petriNet, initialMarking, java.util.UUID.randomUUID())
-
-      instance.marking shouldBe initialMarking
-      Await.result(instance.fireTransition(t1), 2 seconds)
-      instance.accumulatedMarking shouldBe Map(p1 -> Seq(null), p2 -> Seq(null))
-    }
-
-    "should maintain old state when a place is overwritten with new data" in {
-
-      val initialMarking: ColoredMarking = Map(p1 -> Seq(1, 2))
-      val extraData = 3
-
-      val petriNet = process(p1 ~> t1, t1 ~> p2)
-
-      val instance = processInstance(petriNet, initialMarking, java.util.UUID.randomUUID())
-
-      instance.marking shouldBe initialMarking
-      Await.result(instance.fireTransition(t1, Some(extraData)), 2 seconds)
-      instance.accumulatedMarking shouldBe Map(p1 -> Seq(1, 2), p2 -> Seq(3))
     }
   }
 }
