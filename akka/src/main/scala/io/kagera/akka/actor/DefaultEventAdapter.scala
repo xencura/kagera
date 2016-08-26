d969b28b10945175c873b3ff579d77e9825dae7c
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2016-08-26 11:59:53 +0200

    akka; no need to persist unit instances

--- akka/src/main/scala/io/kagera/akka/actor/DefaultEventAdapter.scala
+++ akka/src/main/scala/io/kagera/akka/actor/DefaultEventAdapter.scala
@@ -31,32 +31,40 @@ trait DefaultEventAdapter[S] extends TransitionEventAdapter[S, io.kagera.akka.pe
 
   private lazy val serialization = SerializationExtension.get(system)
 
-  def serializeObject(obj: AnyRef) = {
+  def serializeObject(obj: AnyRef): Option[SerializedData] = {
+    // no need to serialize unit
+    if (obj.isInstanceOf[Unit]) {
+      None
+    } else {
+      // for now we re-use akka Serialization extension for pluggable serializers
+      val serializer = serialization.findSerializerFor(obj)
+      val bytes = serializer.toBinary(obj)
 
-    // for now we re-use akka Serialization extension for pluggable serializers
-    val serializer = serialization.findSerializerFor(obj)
-
-    val bytes = serializer.toBinary(obj)
-
-    // we should not have to copy the bytes
-    SerializedData(serializerId = Some(serializer.identifier), manifest = None, data = Some(ByteString.copyFrom(bytes)))
+      // we should not have to copy the bytes
+      Some(
+        SerializedData(
+          serializerId = Some(serializer.identifier),
+          manifest = None,
+          data = Some(ByteString.copyFrom(bytes))
+        )
+      )
+    }
   }
 
-  def deserializeObject(obj: SerializedData): AnyRef = {
+  def deserializeObject(obj: Option[SerializedData]): AnyRef = {
 
-    obj match {
-      case SerializedData(None, _, Some(data)) =>
-        throw new IllegalStateException(s"Missing serializer id")
-      case SerializedData(None, _, None) =>
-        BoxedUnit.UNIT
-      case SerializedData(Some(serializerId), _, Some(data)) =>
-        val serializer = serialization.serializerByIdentity.getOrElse(
-          serializerId,
-          throw new IllegalStateException(s"No serializer found with id $serializerId")
-        )
-        serializer.fromBinary(data.toByteArray)
-
-    }
+    obj
+      .map {
+        case SerializedData(None, _, Some(data)) =>
+          throw new IllegalStateException(s"Missing serializer id")
+        case SerializedData(Some(serializerId), _, Some(data)) =>
+          val serializer = serialization.serializerByIdentity.getOrElse(
+            serializerId,
+            throw new IllegalStateException(s"No serializer found with id $serializerId")
+          )
+          serializer.fromBinary(data.toByteArray)
+      }
+      .getOrElse(BoxedUnit.UNIT)
   }
 
   override def writeEvent(e: TransitionFired): io.kagera.akka.persistence.TransitionFired = {
@@ -77,7 +85,7 @@ trait DefaultEventAdapter[S] extends TransitionEventAdapter[S, io.kagera.akka.pe
           placeId = Some(place.id.toInt),
           tokenId = Some(tokenIdentifier(place)(value)),
           count = Some(count),
-          tokenData = Some(serializeObject(value.asInstanceOf[AnyRef]))
+          tokenData = serializeObject(value.asInstanceOf[AnyRef])
         )
       }
     }
@@ -88,7 +96,7 @@ trait DefaultEventAdapter[S] extends TransitionEventAdapter[S, io.kagera.akka.pe
       timeCompleted = Some(e.time_completed),
       consumed = consumedTokens,
       produced = producedTokens,
-      data = Some(serializeObject(e.out.asInstanceOf[AnyRef]))
+      data = serializeObject(e.out.asInstanceOf[AnyRef])
     )
 
     protobufEvent
@@ -111,7 +119,7 @@ trait DefaultEventAdapter[S] extends TransitionEventAdapter[S, io.kagera.akka.pe
     }
 
     val produced = e.produced.foldLeft(ColoredMarking.empty) {
-      case (accumulated, ProducedToken(Some(placeId), Some(tokenId), Some(count), Some(data))) =>
+      case (accumulated, ProducedToken(Some(placeId), Some(tokenId), Some(count), data)) =>
         val place = process.places.getById(placeId)
         val value = deserializeObject(data)
         accumulated.add(place.asInstanceOf[Place[Any]], value, count)
@@ -119,7 +127,7 @@ trait DefaultEventAdapter[S] extends TransitionEventAdapter[S, io.kagera.akka.pe
     }
 
     // defaults to Unit instance () in case no data was serialized
-    val data = e.data.map(deserializeObject(_)).getOrElse(())
+    val data = deserializeObject(e.data)
 
     val timeStarted = e.timeStarted.getOrElse(throw new IllegalStateException("Missing field timeStarted"))
 
