8c4945413e2e2219b57c11714e09a7a939891a3d
Merlijn Van Ittersum <merlijn.van.ittersum@trivento.nl>
2016-11-09 10:14:52 +0100

    api; removed unused TransitionFailedException

--- api/src/main/scala/io/kagera/api/colored/TransitionExecutor.scala
+++ api/src/main/scala/io/kagera/api/colored/TransitionExecutor.scala
@@ -16,13 +16,13 @@ trait TransitionExecutor[State] {
   def fireTransition[Input, Output](t: Transition[Input, Output, State]): TransitionFunction[Input, Output, State]
 }
 
-class TransitionExecutorImpl[S](topology: PetriNet[Place[_], Transition[_, _, _]]) extends TransitionExecutor[S] {
+class TransitionExecutorImpl[S](topology: ColoredPetriNet) extends TransitionExecutor[S] {
 
-  val transitionFunctions: Map[Transition[_, _, _], _] =
+  val cachedTransitionFunctions: Map[Transition[_, _, _], _] =
     topology.transitions.map(t => t -> t.apply(topology.inMarking(t), topology.outMarking(t))).toMap
 
-  def tfn[Input, Output](t: Transition[Input, Output, S]): TransitionFunction[Input, Output, S] =
-    transitionFunctions(t).asInstanceOf[TransitionFunction[Input, Output, S]]
+  def transitionFunction[Input, Output](t: Transition[Input, Output, S]) =
+    cachedTransitionFunctions(t).asInstanceOf[TransitionFunction[Input, Output, S]]
 
   def fireTransition[Input, Output](t: Transition[Input, Output, S]): (Marking, S, Input) => Task[(Marking, Output)] = {
     (consume, state, input) =>
@@ -31,12 +31,11 @@ class TransitionExecutorImpl[S](topology: PetriNet[Place[_], Transition[_, _, _]
       }
 
       if (consume.multiplicities != topology.inMarking(t)) {
-        // TODO make more explicit what is wrong here, mention the first multiplicity that is incorrect.
         Task.fail(new IllegalArgumentException(s"Transition $t may not consume $consume"))
       }
 
       try {
-        tfn(t)(consume, state, input).handleWith { handleFailure }
+        transitionFunction(t)(consume, state, input).handleWith { handleFailure }
       } catch { handleFailure }
   }
 }
--- api/src/main/scala/io/kagera/api/colored/TransitionFailedException.scala
+++
@@ -1,6 +0,0 @@
-package io.kagera.api.colored
-
-class TransitionFailedException(message: String, reason: Throwable) extends RuntimeException(message, reason) {
-
-  def this(t: Transition[_, _, _], reason: Throwable) = this(s"Transition $t failed to fire", reason)
-}
