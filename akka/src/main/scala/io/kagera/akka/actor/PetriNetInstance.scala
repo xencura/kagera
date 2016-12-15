345394ce16b1f8bcab3ae23f2758bb58cedaacab
Merlijn van Ittersum <merlijn.van.ittersum@ing.com>
2016-12-15 13:40:47 +0100

    akka; stop the actor when it receives a non Initialize message in 'uninitialized' state

--- akka/src/main/scala/io/kagera/akka/actor/PetriNetInstance.scala
+++ akka/src/main/scala/io/kagera/akka/actor/PetriNetInstance.scala
@@ -74,6 +74,7 @@ class PetriNetInstance[S](
       }
     case msg: Command =>
       sender() ! IllegalCommand("Only accepting Initialize commands in 'uninitialized' state")
+      context.stop(context.self)
   }
 
   def running(instance: Instance[S]): Receive = {
