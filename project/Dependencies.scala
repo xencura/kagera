43f8c860c04149abe69d256b6a7cd69f461d2bb3
Scala Steward <me@scala-steward.org>
2021-09-12 04:48:57 +0200

    Update scalajs-dom to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
 
   val scalaGraph = Def.setting("org.scala-graph" %%% "graph-core" % "1.13.2")
   val scalaGraphDot = "org.scala-graph" %% "graph-dot" % "1.13.0"
-  val scalaJsDom = Def.setting("org.scala-js" %%% "scalajs-dom" % "1.0.0")
+  val scalaJsDom = Def.setting("org.scala-js" %%% "scalajs-dom" % "1.2.0")
   val laminar = Def.setting("com.raquo" %%% "laminar" % "0.11.0")
   val d3 = Def.setting("com.github.xencura.scala-js-d3v4" %%% "scala-js-d3v4" % "766d13e0c1")
   val scalaTags = Def.setting("com.lihaoyi" %%% "upickle" % "1.4.1")
