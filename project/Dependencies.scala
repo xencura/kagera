f9ccece0dd05886a862fc732492c0c1565b25cf6
Scala Steward <me@scala-steward.org>
2021-12-06 20:04:09 +0100

    Update fs2-core to 3.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
   val d3 = Def.setting("com.github.xencura.scala-js-d3v4" %%% "scala-js-d3v4" % "be1e1c8")
   val scalaTags = Def.setting("com.lihaoyi" %%% "scalatags" % "0.11.0")
 
-  val fs2Core = Def.setting("co.fs2" %%% "fs2-core" % "3.2.2")
+  val fs2Core = Def.setting("co.fs2" %%% "fs2-core" % "3.2.3")
   val catsCore = Def.setting("org.typelevel" %%% "cats-core" % "2.7.0")
   val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.3.0")
 
