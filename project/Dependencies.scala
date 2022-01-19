a314d0c9bb0c32a79f99a9af9a8433de40a3953d
Scala Steward <me@scala-steward.org>
2022-01-19 20:41:47 +0100

    Update scalatags to 0.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
   val scalaJsDom = Def.setting("org.scala-js" %%% "scalajs-dom" % "2.1.0")
   val laminar = Def.setting("com.raquo" %%% "laminar" % "0.14.2")
   val d3 = Def.setting("com.github.xencura.scala-js-d3v4" %%% "scala-js-d3v4" % "be1e1c8")
-  val scalaTags = Def.setting("com.lihaoyi" %%% "scalatags" % "0.11.0")
+  val scalaTags = Def.setting("com.lihaoyi" %%% "scalatags" % "0.11.1")
 
   val fs2Core = Def.setting("co.fs2" %%% "fs2-core" % "3.2.4")
   val catsCore = Def.setting("org.typelevel" %%% "cats-core" % "2.7.0")
