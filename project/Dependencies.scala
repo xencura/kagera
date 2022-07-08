d30f78c9465e5cc3b8d62650e2e2b68844a2492a
Scala Steward <scala_steward@virtuslab.com>
2022-07-08 10:47:06 +0000

    Revert commit(s) 2d37c46

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
 
   val fs2Core = Def.setting("co.fs2" %%% "fs2-core" % "3.2.7")
   val catsCore = Def.setting("org.typelevel" %%% "cats-core" % "2.7.0")
-  val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.3.13")
+  val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.3.12")
 
   val zioCore = "dev.zio" %% "zio" % zioVersion
   val zioInteropCats = "dev.zio" %% "zio-interop-cats" % "3.2.9.1"
